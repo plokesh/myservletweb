d0b25824588f09a51848b90f423cf21f3657f093
Lokesh <lokesh.i4u@gmail.com>
2018-10-16 19:48:23 +0530

    Initial commit

---
+++ WebContent/WEB-INF/views/welcome.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home Page</title>
+</head>
+<body>
+
+	<h3>Welcome Page</h3>
+
+	<br>
+	<br>
+	<span>Hi Welcome to Web App</span>
+</body>
+</html>
---
+++ src/com/lok/web/WelcomeServlet.java
@@ -0,0 +1,28 @@
+package com.lok.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/")
+public class WelcomeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 5088319086055123310L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/welcome.jsp");
+		dispatcher.forward(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
